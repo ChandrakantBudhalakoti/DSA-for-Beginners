4425a7586ad8e186456e0eb6370157af0b49e0ee
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-02-15 21:14:22 +0530

    find Fast Exponentiation reduce time Complexity

--- BitManipulation/FastExponentiation.java
+++ BitManipulation/FastExponentiation.java
@@ -1,2 +1,23 @@
 package BitManipulation;
 
+public class FastExponentiation {
+    public static int FastExpo(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans *= a;
+            }
+            a = a * a;
+            n = n >> 1;
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FastExpo(2, 5)); // 32
+        System.out.println(FastExpo(-2, 3)); // -8
+        System.out.println(FastExpo(0, 6)); // 0
+        System.out.println(FastExpo(4, 0)); // 1
+    }
+}
