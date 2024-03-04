4ed760a1719f496887c430724c4f8e331497981e
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-03-04 14:48:01 +0530

    Learning Abstraction

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -1,5 +1,35 @@
 /* Abstraction is a process of hiding the implementation details and showing only functionality to the user.
 Another way, it shows only essential things to the user and hides the internal details*/
 public class Abstraction {
-    
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        h.eat();
+        h.walk();
+        System.out.println("------------------------");
+
+        Chicken c = new Chicken();
+        c.eat();
+        c.walk();
+    }
+
+}
+
+abstract class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    void walk() {
+        System.out.println("Walking on Four legs...");
+    }
+}
+
+class Chicken extends Animal {
+    void walk() {
+        System.out.println("Walking on Two legs...");
+    }
 }
