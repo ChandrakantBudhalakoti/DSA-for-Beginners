2641efe8a2031377c1f7ae70d26f68d9588c7f9a
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-03-04 14:26:26 +0530

    Learning Abstraction

---
+++ OOPS/Abstraction.java
@@ -0,0 +1,5 @@
+/* Abstraction is a process of hiding the implementation details and showing only functionality to the user.
+Another way, it shows only essential things to the user and hides the internal details*/
+public class Abstraction {
+    
+}
