738ef36e32cab08fed2a59d80d43bee34a69e251
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-03-04 23:39:11 +0530

    Learning Interfaces

---
+++ OOPS/Interface.java
@@ -0,0 +1,13 @@
+/*Interface - intreface is the blueprint of the class
+ * we use interfaces for achieving multiple inheritence.
+ * and for the total abstraction .
+ * basic properties for interfaces:-
+ * we create a interface by using interface keyword.
+ * if we wnats to inherit the interface then we use implements keyword instead of extends
+ * all methods are public, abstract & without implementation 
+ * used to achieve total abstraction 
+ * variable in the interface are final, public and static 
+ */
+public class Interface {
+    
+}
