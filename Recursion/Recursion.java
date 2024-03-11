b84bd445f00cabb6fb917482179cbf89737bc0de
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-03-11 20:30:52 +0530

    solve some problems

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -4,21 +4,66 @@ package Recursion;
  * Recursion
  */
 public class Recursion {
-    public static void printDec(int n){
-        //base case : if the number is 1, just return.
-        if (n == 1){
+    // Problem 1: Print numbers in decreasing order
+    public static void printDec(int n) {
+        // base case : if the number is 1, just return.
+        if (n == 1) {
             System.out.print(n);
             return;
         }
-        //recursive case: call the function with n-1 and print n in between of that call.
+        // recursive case: call the function with n-1 and print n in between of that
+        // call.
         System.out.print(n + " ");
         printDec(n - 1);
-
-    }
-    public static void main(String[] args) {
-        int n = 10;
-        printDec(n);
     }
 
+    // Problem 2: Print numbers in increasing order
+    public static void printInc(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n - 1);
+        System.out.print(n + " ");
+
+    }
+
+    // Problem 3: Print Factorial of a number n
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers");
+            return -1;
+        }
+        int fnm1 = factorial(n - 1);
+        int fn = n * fnm1;
+        return fn;
+    }
+
+    // Problem 3: Print sum of first n natural numbers
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int snm1 = sum(n - 1);
+        int sn = n + snm1;
+        return sn;
+    }
+
+    //Problem 3: Print Nth Fibbonacci number
     
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print("Decreasing Order: ");
+        printDec(n);
+        System.out.println();
+        System.out.print("Increasing Order: ");
+        printInc(n);
+        System.out.println();
+        // System.out.println("Factorial of n");
+        System.out.println("Factorial of " + n + "! = " + factorial(n));
+        System.out.println("Sum of first " + n + " natural numbers = " + sum(n));
+    }
 }
