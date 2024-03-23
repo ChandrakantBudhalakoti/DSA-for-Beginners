840915066ab047c667aa6e8fddc16885dc9058bf
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-03-23 21:20:42 +0530

    solve Remove duplicate Strings problem

--- Recursion/RecursionAdvance.java
+++ Recursion/RecursionAdvance.java
@@ -15,8 +15,30 @@ public class RecursionAdvance {
         int totalWays = fnm1 + fnm2;
         return totalWays;
     }
+
+    //Problem 2: Remove duplicate Strings 
+    public static void removeDuplicateStrings(String str, int idx, StringBuilder newStr, boolean[] map){
+        //base case 
+        if(idx == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        //recursive case
+        char currentChar = str.charAt(idx);
+        //check if the character is already added or not
+        if(map[currentChar - 'a'] == true){
+            //duplicate so skip this character and move to next index
+            removeDuplicateStrings(str, idx+1, newStr, map);
+        }else{
+            map[currentChar - 'a'] = true;
+            removeDuplicateStrings(str, idx+1, newStr.append(currentChar), map);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Number of ways to tile a room with n tiles is : "+tillingProblem(4));
+        String str = "appnnacollege";
+        boolean[] map = new boolean[26];//assuming only lowercase alphabets are there in string
+        removeDuplicateStrings(str, 0, new StringBuilder(), map);
     }
     
 }
