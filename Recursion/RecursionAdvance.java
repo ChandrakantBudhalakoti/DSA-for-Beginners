670d842dbaac425983fd2db36d93434e29ccf8e2
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-03-23 20:35:30 +0530

    update RecursionAdvance.java

--- Recursion/RecursionAdvance.java
+++ Recursion/RecursionAdvance.java
@@ -1,5 +1,18 @@
 package Recursion;
 
 public class RecursionAdvance {
+    // Problem 1: Tilling Porblem 
+    public static int tillingProblem(int n){
+        //base case 
+        if(n==0 || n==1){
+            return 1;
+        }
+        //recursive case
+        //if vertical direction choice
+        int fnm1 = tillingProblem(n-1);
+        int fnm2 = tillingProblem(n-2);
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+    }
     
 }
