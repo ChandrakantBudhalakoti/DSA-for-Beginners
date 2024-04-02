4f8800a7b8b5d146598e05e0cc544a2a150b87e9
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-02 13:00:36 +0530

    implement modifiedBubbleSort

--- Arrays/Sorting/modifiedBubbleSort.java
+++ Arrays/Sorting/modifiedBubbleSort.java
@@ -5,17 +5,25 @@ public class ModifiedBubbleSort {
         for (int turn = 0; turn < arr.length - 1; turn++) {
             boolean swapped = false;
             for (int j = 0; j < arr.length - 1 - turn; j++) {
-                if(arr[j] > arr[j + 1]) {
-                    //swapped
+                if (arr[j] > arr[j + 1]) {
+                    // swapped
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
                 }
             }
-            if(swapped == false){
+            if (swapped == false) {
                 break;
             }
         }
+    }
+    public static void main (String[] args){
+        int[] arr = { 5, 4, 1, 3, 2 };
+        modifiedBubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
