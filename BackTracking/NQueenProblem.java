5415a5a81f8405c555ede9af3c4bda5a49a78a67
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-07 21:16:39 +0530

    add main Method

--- BackTracking/NQueenProblem.java
+++ BackTracking/NQueenProblem.java
@@ -49,4 +49,16 @@ public class NQueenProblem {
             System.out.println();
         }
     }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+        // initialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        nQueen(board, 0);
+    }
 }
