b60aac4337c423ff1a268ba72036ae57b6ffe276
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-07 21:06:16 +0530

    add safe moves in NQueenProblem

--- BackTracking/NQueenProblem.java
+++ BackTracking/NQueenProblem.java
@@ -1,8 +1,27 @@
 package BackTracking;
 
 public class NQueenProblem {
-    public static boolean isSafe(char board [][], int row, int col){
-        
+    //all posible combination for queens safe location
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // diagonal left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // diagonal right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
 }
