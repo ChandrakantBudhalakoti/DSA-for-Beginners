489817038cef29642819643d9b1429557baaa0c2
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-09 21:35:57 +0530

    solve grid ways problem

--- BackTracking/GridWays.java
+++ BackTracking/GridWays.java
@@ -1,5 +1,20 @@
 package BackTracking;
 
 public class GridWays {
-    
+    public static int gridWays(int i, int j, int n, int m) {
+        // base case
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (i == n || j == n) {
+            return 0;
+        }
+        // recursive case
+        int w1 = gridWays(i + 1, j, n, m); // right move
+        int w2 = gridWays(i, j + 1, n, m); // down move
+        return w1 + w2;
+    }
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.println(gridWays(0, 0, n, m));
+    }
 }
