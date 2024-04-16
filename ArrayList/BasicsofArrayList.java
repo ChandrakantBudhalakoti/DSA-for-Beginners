672cb16eb2d4413b4a99222468141bdce7e41499
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-16 22:05:35 +0530

    Add operation

--- ArrayList/BasicsofArrayList.java
+++ ArrayList/BasicsofArrayList.java
@@ -3,6 +3,18 @@ import java.util.ArrayList;
 public class BasicsofArrayList {
     public static void main(String[] args) {
         //ArrayList Creation 
+        ArrayList <Integer> List = new ArrayList<>();
+        //1st Add operation
+        List.add(1);
+        List.add(2);
+        List.add(3);
+        List.add(4);
+        List.add(5);
+        System.out.println(List);//[1, 2,  3, 4, 5]
+        //Add at specific position 
+        List.add(0, 14);
+        System.out.println(List);
+
     }
     
 }
