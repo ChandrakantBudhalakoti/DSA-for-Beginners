04cc554ecf395791e3071ec3b122b3c929e36435
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-17 22:52:32 +0530

    ProblemsofArrayList.java

---
+++ ArrayList/ProblemsofArrayList.java
@@ -0,0 +1,43 @@
+package ArrayList;
+
+import java.util.ArrayList;
+// import java.util.List;
+
+public class ProblemsofArrayList {
+    // problem 1. reverse arraylist
+    public static void reverse(ArrayList<Integer> List) {
+        System.out.println("Original List : ");
+        for (int i : List) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nReversed List : ");
+        for (int i = List.size(); i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    //problem 2 find maximum in an arraylist
+    public static int maxElement(ArrayList<Integer> List) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < List.size(); i++) {
+            if(max < List.get(i)){
+                max = List.get(i);
+            }
+        }
+        System.out.println(max);
+        return max;
+            
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(1);
+        List.add(2);
+        List.add(3);
+        List.add(4);
+        List.add(5);
+        // reverse(List);
+        maxElement(List);
+    }
+
+}
