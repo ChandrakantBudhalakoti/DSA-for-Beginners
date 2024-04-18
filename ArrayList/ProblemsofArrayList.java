0bfc04f665c3833ce2326dc968dbfa243e446b0b
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-18 22:11:28 +0530

    add swap two numbers in arraylist

--- ArrayList/ProblemsofArrayList.java
+++ ArrayList/ProblemsofArrayList.java
@@ -30,6 +30,14 @@ public class ProblemsofArrayList {
         return max;
             
     }
+    
+    //problem 2 swap two numbers in an arraylist
+    public static void swap (ArrayList<Integer>list, int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> List = new ArrayList<>();
         List.add(1);
@@ -40,6 +48,18 @@ public class ProblemsofArrayList {
         List.add(55);
         reverse(List);
         maxElement(List);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        int idx1 = 1, idx2 = 3;
+        System.out.println("Before swapping elements " + list );
+        swap(list, idx1, idx2);
+        System.out.println("After swapping elements " + list );
+
     }
 
 }
