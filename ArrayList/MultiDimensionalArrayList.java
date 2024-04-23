470a66e2e33715193bb4a77a6ea3db70f180eaa6
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-23 08:55:58 +0530

    update MultiDimensionalArrayList.java

--- ArrayList/MultiDimensionalArrayList.java
+++ ArrayList/MultiDimensionalArrayList.java
@@ -17,6 +17,15 @@ public class MultiDimensionalArrayList {
         mainList.add(list2);
         mainList.add(list3);
         System.out.println(mainList);
+
+        //nested loop for print speratly 
+        for(int i = 0; i< mainList.size(); i++){
+            ArrayList<Integer>currList = mainList.get(i);
+            for(int j = 0; j<currList.size(); j++){
+                System.out.print(currList.get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
