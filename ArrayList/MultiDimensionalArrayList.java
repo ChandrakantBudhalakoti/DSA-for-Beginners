1500df8fdb95cb8268ff1208ce3dc4b02af2a0f6
ChandrakantBudhalakoti <chandrakantbudhalakoti189@gmail.com>
2024-04-23 08:37:32 +0530

    MultiDimensionalArrayList.java

--- ArrayList/MultiDimensionalArrayList.java
+++ ArrayList/MultiDimensionalArrayList.java
@@ -1,5 +1,34 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class MultiDimensionalArrayList {
-    
+    public static void main(String[] args) {
+        // Create a two-dimensional array list with 5 rows and 3 columns.
+        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        mainList.add(list1);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(4);
+        list2.add(6);
+        list2.add(8);
+        mainList.add(list2);
+        
+        for (int i = 0 ; i< mainList.size(); i++){
+            ArrayList<Integer>currList = mainList.get(i);
+            for(int j = 0;  j<currList.size(); j++){
+                System.out.print(currList.get(j) + " ");
+                // System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println(mainList);
+
+    }
+
 }
